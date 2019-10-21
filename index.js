dcd0ca347e480277425aa30ea98fed5dd100b496
amusing-transpiler-2573 <anil_grg@hotmail.com>
2019-10-21 15:10:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+kittens. push ("lado");
+console.log("kittens");
 
 // Add your functions and code here
